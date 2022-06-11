b4ab4b8574f3f3445fd24cdad1885e0ad400e65a
Matthew Henderson <info@matthewahenderson.com>
2022-06-11 13:20:58 -0400

    Added maze dimensions

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } = Matter;
+const {
+    Engine,
+    Render, 
+    Runner, 
+    World, 
+    Bodies
+} = Matter;
 
-const width = 800;
+const cells = 3;
+const width = 600;
 const height = 600;
 
 const engine = Engine.create();
@@ -9,6 +16,7 @@ const render = Render.create({
     element: document.body,
     engine: engine,
     options: {
+        wireframes: true,
         width,
         height
     }
@@ -17,32 +25,26 @@ const render = Render.create({
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-World.add(
-    world,
-    MouseConstraint.create(engine, {
-        mouse: Mouse.create(render.canvas)
-}));
-
 // Create walls and add to world
 const walls = [
-    Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
-    Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
-    Bodies.rectangle(0, 300, 40, 600, { isStatic: true }),
-    Bodies.rectangle(800, 300, 40, 600, { isStatic: true })
+    Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
+    Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
+    Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
+    Bodies.rectangle(width, height / 2, 40, height, { isStatic: true })
 ];
 World.add(world, walls);
 
-// Generate random objects
-for (let i = 0; i < 20; i++) {
-    if (Math.random() > 0.5) {
-        World.add(
-            world,
-            Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50)
-        );
-    } else (
-        World.add(
-            world,
-            Bodies.circle(Math.random() * width, Math.random() * height, 35)
-        )
-    )    
-}
+// Maze generation
+const grid = Array(cells)
+    .fill(null)
+    .map(() => Array(cells).fill(false));
+
+const verticals = Array(cells)
+    .fill(null)
+    .map(() => Array(cells - 1).fill(false));
+
+const horizontals = Array(cells - 1)
+    .fill(null)
+    .map(() => Array(cells).fill(false));
+
+
