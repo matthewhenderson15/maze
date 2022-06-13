a04da03808911ad6ed32e71ab3f485911e64c71b
Matthew Henderson <info@matthewahenderson.com>
2022-06-12 20:12:57 -0400

    Modified margins to stretch canvas; added logic to move ball and win game

--- index.js
+++ index.js
@@ -3,16 +3,19 @@ const {
     Render, 
     Runner, 
     World, 
-    Bodies
+    Bodies,
+    Body,
+    Events
 } = Matter;
 
-const cells = 3;
-const width = 600;
-const height = 600;
+const cells = 6;
+const width = window.innerWidth;
+const height = window.innerHeight;
 
 const unitLength = width / cells;
 
 const engine = Engine.create();
+engine.world.gravity.y = 0;
 const { world } = engine;
 const render = Render.create({
     element: document.body,
@@ -29,10 +32,10 @@ Runner.run(Runner.create(), engine);
 
 // Create walls and add to world
 const walls = [
-    Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
-    Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
-    Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
-    Bodies.rectangle(width, height / 2, 40, height, { isStatic: true })
+    Bodies.rectangle(width / 2, 0, width, 2, { isStatic: true }),
+    Bodies.rectangle(width / 2, height, width, 2, { isStatic: true }),
+    Bodies.rectangle(0, height / 2, 2, height, { isStatic: true }),
+    Bodies.rectangle(width, height / 2, 2, height, { isStatic: true })
 ];
 World.add(world, walls);
 
@@ -132,6 +135,7 @@ horizontals.forEach((row, rowIndex) => {
             unitLength,
             5,
             {
+                label: 'wall',
                 isStatic: true
             }
         );
@@ -151,9 +155,70 @@ verticals.forEach((row, rowIndex) => {
             5,
             unitLength,
             {
+                label: 'wall',
                 isStatic: true
             }
         );
         World.add(world, wall);
+    });
+});
+
+// Create the goal
+const goal = Bodies.rectangle(
+    width - unitLength / 2,
+    height - unitLength / 2,
+    unitLength * .7,
+    unitLength * .7,
+    {
+        label: 'goal',
+        isStatic: true
+    }
+);
+World.add(world, goal);
+
+// Create the ball
+const ball = Bodies.circle(
+    unitLength / 2,
+    unitLength / 2,
+    unitLength / 4,
+    {
+        label: 'ball'
+    }
+);
+World.add(world, ball);
+
+document.addEventListener('keydown', event => {
+    const { x, y } = ball.velocity;
+    
+    if (event.keyCode === 87) {
+        Body.setVelocity(ball, { x, y: y - 5 });
+    }
+
+    if (event.keyCode === 68) {
+        Body.setVelocity(ball, { x: x + 5, y });
+    }
+
+    if (event.keyCode === 83) {
+        Body.setVelocity(ball, { x, y: y + 5 });
+    }
+
+    if (event.keyCode === 65) {
+        Body.setVelocity(ball, { x: x - 5, y });
+    }
+});
+
+// Win condition
+Events.on(engine, 'collisionStart', event => {
+    event.pairs.forEach(collision => {
+        const labels = ['ball', 'goal'];
+        
+        if (labels.includes(collision.bodyA.label) && labels.includes(collision.bodyB.label)) {
+            world.gravity.y = 1;
+            world.bodies.forEach(body => {
+                if (body.label === 'wall') {
+                    Body.setStatic(body, false);
+                }
+            });
+        }
     });
 });
